ce9b5b3bf4b0858d6d311dd600bc681006ccca25
Yohai Wiener <yohaiwiener@gmail.com>
2024-01-11 17:12:24 +0200

    Add print reminder dialog to ContactMeModal component

--- src/components/ContactMeModal.tsx
+++ src/components/ContactMeModal.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { IoIosMail } from "react-icons/io";
 import * as IGLogo from "../../public/instagram.png";
+import * as PrintTip from "../../public/howToPrint.png";
+import Spacer from "./utility/Spacer";
 interface Props {
   showContact: boolean;
 }
 
 const ContactMeModal = ({ showContact }: Props) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
   return (
     <div
       style={{
@@ -15,9 +18,9 @@ const ContactMeModal = ({ showContact }: Props) => {
         transformOrigin: "bottom right",
         transition: "all 0.150s ease",
       }}
-      className="absolute bottom-10 right-5 w-48 flex-col rounded-[10px] bg-white p-4 shadow-xl"
+      className="absolute bottom-10 right-5 flex w-[16rem] flex-col items-center rounded-[10px] bg-white p-4 shadow-xl"
     >
-      <h4 className="self-center text-center font-semibold">Contact Me</h4>
+      <h3 className="self-center text-center font-semibold">Contact Me</h3>
 
       <div className="flex flex-row items-center justify-between">
         <a type="email" href="mailto:yohaiwiener@gmail.com">
@@ -31,6 +34,33 @@ const ContactMeModal = ({ showContact }: Props) => {
           <Image alt="Instagram logo" src={IGLogo} width={50} height={50} />
         </a>
       </div>
+
+      <button
+        onClick={() => dialogRef.current?.showModal()}
+        className="mt-4 self-center rounded-[10px] bg-primary p-4 text-white transition-all duration-150 active:scale-95"
+      >
+        Remind me how to print
+      </button>
+
+      <dialog
+        ref={dialogRef}
+        className="h-3/5 w-1/4 overflow-hidden rounded-[10px] p-4 shadow-xl transition-all duration-150"
+      >
+        <div className="flex h-full flex-1 flex-col items-center justify-start">
+          <h4 className="w-3/4">
+            After clicking <b>ctrl + P</b> or <b>cmd + P</b> make sure to
+            unselect "Headers and Footers" and select "Background Graphics"
+          </h4>
+          <Image src={PrintTip} alt="How to print" />
+          <Spacer />
+          <button
+            onClick={() => dialogRef.current?.close()}
+            className="mt-4 self-center rounded-[10px] bg-primary p-4 text-white transition-all duration-150 active:scale-95"
+          >
+            Got it!
+          </button>
+        </div>
+      </dialog>
     </div>
   );
 };
