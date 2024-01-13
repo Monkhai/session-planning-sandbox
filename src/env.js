311e633c319d4a76f3ac6fc7b1157bd07f1c3cd8
Yohai Wiener <yohaiwiener@gmail.com>
2024-01-13 20:34:36 +0200

    Remove unused environment variable

--- src/env.js
+++ src/env.js
@@ -9,14 +9,12 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-    NEXT_PUBLIC_STATIC_USER_ID: z.string(),
   },
 
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    NEXT_PUBLIC_STATIC_USER_ID: process.env.NEXT_PUBLIC_STATIC_USER_ID,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 
