ec8f65a6f4d3f6c92a2f730d5766692992b77542
Yohai Wiener <yohaiwiener@gmail.com>
2024-01-31 12:52:38 +0200

    Add HelpButton component and handle logout

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 import { PiDotsThreeCircleFill } from "react-icons/pi";
 import CreateNewSessionButton from "~/components/CreateNewSessionButton";
+import HelpButton from "~/components/HelpButton";
 import Loader from "~/components/Loader";
 import NavBar from "~/components/NavBar";
 import SessionList from "~/components/Sessions/SessionList";
@@ -11,13 +13,23 @@ import SessionRow from "~/components/Sessions/SessionRow";
 import Spacer from "~/components/utility/Spacer";
 import useCreateSession from "~/hooks/sessionsHooks/useCreateSession";
 import useGetSessions from "~/hooks/sessionsHooks/useGetSessions";
+import client from "~/utils/supabaseClient";
 
 const page = () => {
+  const [showContact, setShowContact] = React.useState(false);
+
+  const router = useRouter();
+
   const { data: sessions, isLoading } = useGetSessions();
   const { mutate: createNewSession } = useCreateSession();
 
   const handleCreateNewSession = (name: string) => {
     createNewSession({ name: name, lastOrder: sessions?.length || 0 });
+  };
+
+  const handleLogout = () => {
+    void client.auth.signOut();
+    router.replace("/login");
   };
 
   if (isLoading) {
@@ -37,7 +49,13 @@ const page = () => {
       <Spacer />
 
       <div className="sticky bottom-0 flex w-full flex-row items-center justify-center gap-4 bg-[rgba(215,215,215,0.5)] px-4 py-2 backdrop-blur-md print:hidden md:bottom-0 md:px-10 md:py-5 dark:bg-opacNavbarBackground">
+        <Spacer />
         <CreateNewSessionButton onCreateNewSession={handleCreateNewSession} />
+        <HelpButton
+          onLogout={handleLogout}
+          setShowContact={setShowContact}
+          showContact={showContact}
+        />
       </div>
     </main>
   );
