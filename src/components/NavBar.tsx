981da5655c0322136bf16ad3f75b2e6f7a062f34
Yohai Wiener <yohaiwiener@gmail.com>
2024-04-14 19:32:30 +0300

    Update NavBar component with logo and styling changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Spacer from "./utility/Spacer";
 import { usePathname, useRouter } from "next/navigation";
 import { IoChevronBack } from "react-icons/io5";
+import logo from "../../public/dark-logo.png";
+import Image from "next/image";
 
 interface Props {
   title?: string;
@@ -16,7 +18,7 @@ const NavBar = ({ title = "FlexiPlan" }: Props) => {
   const isHome = currentRoute === "/home";
 
   return (
-    <nav className="sticky bottom-0 flex w-full flex-row items-center justify-center gap-4 bg-[rgba(215,215,215,0.5)]  backdrop-blur-md print:hidden  dark:bg-opacNavbarBackground">
+    <nav className="sticky top-0 z-50 flex w-full flex-row items-center justify-center border-2 bg-navbarBackground print:hidden dark:bg-darkSecondaryBackground">
       {!isHome ? (
         <button
           className=" flex flex-1 flex-row items-center"
@@ -26,10 +28,19 @@ const NavBar = ({ title = "FlexiPlan" }: Props) => {
           <h3 className="text-white">Back</h3>
         </button>
       ) : null}
-      <Link className="flex-[ 2 ] flex justify-center" href="/home">
-        <h1 className="p-5 text-white">{title}</h1>
-      </Link>
 
+      <div className="flex flex-row">
+        <Link className="flex-[ 2 ] flex justify-center" href="/home">
+          <h1 className="p-5 text-white">{title}</h1>
+        </Link>
+        <Image
+          src={logo}
+          alt="FlexiPlan Logo"
+          className="object-contain"
+          width={50}
+          height={50}
+        />
+      </div>
       {!isHome ? <Spacer /> : null}
     </nav>
   );
